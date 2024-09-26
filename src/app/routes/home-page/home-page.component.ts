480fdf17b4f7f58a7e4d60e73757e7574f849310
Ahmad <ahmadhijazi_me@hotmail.com>
2024-09-26 16:23:10 +0200

    Form Implementation

--- src/app/routes/home-page/home-page.component.ts
+++ src/app/routes/home-page/home-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {FooterComponent} from "../../shared/ui/footer/footer.component";
 import {TopBarComponent} from "../../shared/ui/top-bar/top-bar.component";
+import {RegisterFormComponent} from "../../shared/ui/register-form/register-form.component";
+import {DataTableComponent} from "../../shared/ui/data-table/data-table.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [TopBarComponent, FooterComponent],
+  imports: [TopBarComponent, FooterComponent, RegisterFormComponent, DataTableComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
---
+++ src/app/shared/ui/register-form/register-form.component.ts
@@ -0,0 +1,33 @@
+import {Component, inject} from '@angular/core';
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
+
+
+@Component({
+  selector: 'app-register-form',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  templateUrl: './register-form.component.html',
+  styleUrl: './register-form.component.css'
+})
+export class RegisterFormComponent {
+  private _formBuilder = inject(FormBuilder);
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  isLinear = false;
+}
